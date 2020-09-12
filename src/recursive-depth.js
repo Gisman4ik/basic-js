526c965622b2ddb519ce433b1e44bdfdecffcdd5
Gisman4ik <Sftawitkakpro@gmail.com>
2020-09-12 22:50:27 +0300

    feat: complete recursive

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,17 +1,19 @@
 const CustomError = require("../extensions/custom-error");
-
 module.exports = class DepthCalculator {
-  depth = 0;  
   calculateDepth = ( arr ) => {
     if(Array.isArray(arr)) {
-      this.depth++;
-      for (let i = 0; i < arr.length; i++) {
+      let comDepth=1;
+      for (let i = 0; i < arr.length || i<1; i++) {
+        let locDepth=1;
         if(Array.isArray(arr[i])) {
-          console.log(this.depth, arr[i])
-          this.calculateDepth(arr[i]);
+          locDepth += this.calculateDepth(arr[i]);
         }
+        if(locDepth > comDepth) {
+          comDepth = locDepth
+        };
+        locDepth=0;
       }  
+      return comDepth ;
     }
-    return this.depth;
   }
 };
